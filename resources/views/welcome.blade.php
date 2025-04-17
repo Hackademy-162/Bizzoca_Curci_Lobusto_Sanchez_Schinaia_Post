c71cec6304353cd8a7d1a72920f7552282d7cf1c
Mariano Curci <curci.mariano2003@gmail.com>
2025-04-17 15:03:32 +0200

    aggiunti componenti

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-    
-</body>
-</html>
+<x-layout>
+    <x-navbar/>
+    <h1>Ciao</h1>
+    <x-footer/>
+</x-layout>
