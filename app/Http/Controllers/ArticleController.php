8ec9fca76b0afd7b0013949a798cb356547c7904
Domenico Lobusto <domenico.lobusto86@gmail.com>
2025-04-18 19:59:38 +0200

    aggiunti creazioni modelli Category e article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ArticleController;
 
 class ArticleController extends Controller
@@ -12,5 +14,32 @@ class ArticleController extends Controller
         return view('welcome');
     }
 
+    public function create()
+    {
+        return view('article.create');
+    }
+
+    public function store(Request $request)
+
+    {
+        $request->validate([
+            'title'    => 'required|unique:articles|min:5',
+            'subtitle' => 'required|min:5',
+            'body'     => 'required|min:10',
+            'image'    => 'required|image',
+            'category' => 'required',
+        ]);
+    
+        $article = Article::create([
+            'title'      => $request->title,
+            'subtitle'   => $request->subtitle,
+            'body'       => $request->body,
+            'image'      => $request->file('image')->store('images', 'public'),
+            'category_id'=> $request->category,
+            'user_id'    => Auth::user()->id,
+        ]);
+    
+        return redirect(route('homepage'))->with('message', 'Articolo creato con successo');
+    }
     
 }
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -32,12 +32,14 @@
             
           </ul>
         </li>
+        <li class="nav-item">
+
+          <a class="nav-link" href="{{route('article.create')}}">Inserisci un articolo</a>
+          
+        </li>
         @endauth
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      
     </div>
   </div>
 </nav>
